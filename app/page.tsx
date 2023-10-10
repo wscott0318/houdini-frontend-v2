abb7b180b0e53607b482f86864c85cd26a867fb4
Raul Gavris <rg.raulgavris@gmail.com>
2023-10-10 21:14:20 +0300

    format

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import Image from 'next/image'
+import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="text-9xl">Hello new Houdini!</div>
     </main>
-  )
+  );
 }
