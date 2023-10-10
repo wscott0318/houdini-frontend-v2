f34a85d1d773c9e2f60758a0fb0a63999ccb497b
Raul Gavris <rg.raulgavris@gmail.com>
2023-10-10 23:52:38 +0300

    some fixes

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+'use client'
+import { HoudiniButton } from '@/node_modules/react-sdk'
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="text-9xl">Hello new Houdini!</div>
+      <HoudiniButton />
     </main>
   );
 }
