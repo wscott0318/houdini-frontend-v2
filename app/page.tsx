3a27e88c29addc4eac54fdc4bd106e2f49e05707
Raul Gavris <rg.raulgavris@gmail.com>
2023-10-18 12:05:02 +0300

    svgs and some text

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,43 @@
 'use client'
 
+import {
+  ArcSvg,
+  BoltbotSvg,
+  BulletSvg,
+  FiraSvg,
+  Protocol1Svg,
+  Protocol2Svg,
+  Protocol3Svg,
+  Protocol4Svg,
+  Protocol5Svg,
+  Protocol6Svg,
+  Protocol7Svg,
+  Protocol8Svg,
+  Protocol9Svg,
+  Protocol10Svg,
+  Protocol11Svg,
+  Protocol12Svg,
+  Protocol13Svg,
+  Protocol14Svg,
+  Protocol15Svg,
+  Protocol16Svg,
+  Protocol17Svg,
+  Protocol18Svg,
+  Protocol19Svg,
+  Protocol20Svg,
+  Protocol21Svg,
+  Protocol22Svg,
+  Protocol23Svg,
+  Protocol24Svg,
+  Protocol25Svg,
+  Protocol26Svg,
+  Protocol27Svg,
+  Protocol28Svg,
+  Protocol29Svg,
+  SwiftSvg,
+  XenifySvg,
+} from '@/components/Svg'
+
 export default function Home() {
   return (
     <div className="row-start-2 row-end-3 flex flex-col gap-[30px] md:gap-[80px] justify-center items-center my-[80px] sm:px-0 px-6">
@@ -31,14 +69,80 @@ export default function Home() {
       </div>
 
       <div className="flex flex-col w-full lg:flex-row justify-center items-center gap-4">
-        <div className="w-[260px] h-[140px] bg-gray-500 sm:block hidden"></div>
-        <div className="w-full md:w-[350px] h-[190px] bg-gray-500"></div>
-        <div className="w-[260px] h-[140px] bg-gray-500 sm:block hidden"></div>
+        <div className="w-[260px] h-[140px] bg-gray-500 sm:flex hidden flex-col justify-center items-center">
+          <div className="text-[23px] rainbow-text leading-[34px] text-center font-light">
+            Total Transactions
+          </div>
+          <div className="text-[35px] text-white leading-[50px] font-medium text-center">
+            123k
+          </div>
+        </div>
+        <div className="w-full md:w-[350px] h-[190px] bg-gray-500 flex flex-col justify-center items-center">
+          <div className="text-[25px] font-medium text-center leading-[38px]">Total Swap Volume</div>
+          <div className="text-[35px] text-white leading-[50px] font-medium text-center">
+            123
+          </div>
+        </div>
+        <div className="w-[260px] h-[140px] bg-gray-500 sm:flex hidden flex-col justify-center items-center">
+          <div className="text-[23px] rainbow-text leading-[34px] text-center font-light">
+            $POOF Buybacks
+          </div>
+          <div className="text-[35px] text-white leading-[50px] font-medium text-center">
+            5.7m
+          </div>
+        </div>
       </div>
 
-      <div className="flex flex-col items-centers justify-center gap-2 bg-gray-500 w-full md:w-[650px] lg:w-[850px] h-[230px]"></div>
+      <div className="flex flex-col items-centers justify-center gap-5 bg-gray-500 w-full md:w-[650px] lg:w-[850px] h-[230px] py-8 px-20">
+        <div className="text-[20px] gradient-text leading-[24px] text-center font-medium">
+          Partners
+        </div>
+        <div className="flex flex-row items-start justify-center gap-[60px] flex-wrap">
+          <BulletSvg className="w-[133px] h-[28px]" />
+          <FiraSvg className="w-[133px] h-[35px]" />
+          <ArcSvg className="w-[133px] h-[35px]" />
+          <BoltbotSvg className="w-[133px] h-[32px]" />
+          <XenifySvg className="w-[133px] h-[28px]" />
+          <SwiftSvg className="w-[133px] h-[35px]" />
+        </div>
+      </div>
 
-      <div className="flex flex-col items-centers justify-center gap-2 bg-gray-500 w-full md:w-[600px] h-[350px]"></div>
+      <div className="flex flex-col items-centers justify-start gap-10 bg-gray-500 w-full md:w-[600px] h-[350px] py-8 px-20">
+        <div className="text-[20px] text-[#8C9AE9] leading-[24px] text-center font-medium">
+          Supported Protocols
+        </div>
+        <div className="flex flex-row items-start justify-center gap-[25px] flex-wrap">
+          <Protocol1Svg className="w-[32px] h-[32px]" />
+          <Protocol2Svg className="w-[32px] h-[32px]" />
+          <Protocol3Svg className="w-[32px] h-[32px]" />
+          <Protocol4Svg className="w-[32px] h-[32px]" />
+          <Protocol5Svg className="w-[32px] h-[32px]" />
+          <Protocol6Svg className="w-[32px] h-[32px]" />
+          <Protocol7Svg className="w-[32px] h-[32px]" />
+          <Protocol8Svg className="w-[32px] h-[32px]" />
+          <Protocol9Svg className="w-[32px] h-[32px]" />
+          <Protocol10Svg className="w-[32px] h-[32px]" />
+          <Protocol11Svg className="w-[32px] h-[32px]" />
+          <Protocol12Svg className="w-[32px] h-[32px]" />
+          <Protocol13Svg className="w-[32px] h-[32px]" />
+          <Protocol14Svg className="w-[32px] h-[32px]" />
+          <Protocol15Svg className="w-[32px] h-[32px]" />
+          <Protocol16Svg className="w-[32px] h-[32px]" />
+          <Protocol17Svg className="w-[32px] h-[32px]" />
+          <Protocol18Svg className="w-[32px] h-[32px]" />
+          <Protocol19Svg className="w-[32px] h-[32px]" />
+          <Protocol20Svg className="w-[32px] h-[32px]" />
+          <Protocol21Svg className="w-[32px] h-[32px]" />
+          <Protocol22Svg className="w-[32px] h-[32px]" />
+          <Protocol23Svg className="w-[32px] h-[32px]" />
+          <Protocol24Svg className="w-[32px] h-[32px]" />
+          <Protocol25Svg className="w-[32px] h-[32px]" />
+          <Protocol26Svg className="w-[32px] h-[32px]" />
+          <Protocol27Svg className="w-[32px] h-[32px]" />
+          <Protocol28Svg className="w-[32px] h-[32px]" />
+          <Protocol29Svg className="w-[32px] h-[32px]" />
+        </div>
+      </div>
     </div>
   )
 }
