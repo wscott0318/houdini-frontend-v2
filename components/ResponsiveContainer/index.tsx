dcfd1f80b3f253fc0509e35f465789142f23ab67
Raul Gavris <rg.raulgavris@gmail.com>
2023-10-25 13:09:11 +0300

    small fixes

--- components/ResponsiveContainer/index.tsx
+++ components/ResponsiveContainer/index.tsx
@@ -1,11 +1,9 @@
 'use client'
 
-export function ResponsiveContainer({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export function ResponsiveContainer({ children }: LayoutProps) {
   return (
-    <main className="grid grid-cols-1 gap-4 pt-10 px-4 lg:px-10">{children}</main>
+    <main className="grid grid-cols-1 gap-4 pt-10 px-4 lg:px-10">
+      {children}
+    </main>
   )
 }
