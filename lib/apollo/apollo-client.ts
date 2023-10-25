20d35352d3908c152bc7042e82f8569f0f2d7d12
ens53712 <ens53712@adobe.com>
2023-10-25 22:47:26 +0300

    fix: made apollo-upload-client work with v18

--- lib/apollo/apollo-client.ts
+++ lib/apollo/apollo-client.ts
@@ -1,6 +1,6 @@
-import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client'
+import { ApolloClient, ApolloLink, InMemoryCache, createHttpLink } from '@apollo/client'
 import { setContext } from '@apollo/client/link/context'
-// import { createUploadLink } from 'apollo-upload-client'
+import createUploadLink from "apollo-upload-client/createUploadLink.mjs";
 
 const httpUserLink = createHttpLink({
   uri: process.env.NEXT_APP_GQL_USER_API ?? 'http://localhost:3000/graphql',
@@ -14,20 +14,15 @@ const authUserLink = setContext((_, { headers }) => {
   }
 })
 
-// const uploadLink = createUploadLink({
-//   uri: process.env.NEXT_APP_GQL_USER_API ?? 'http://localhost:3000/graphql',
-//   credentials: 'include',
-// })
+const uploadLink = createUploadLink({
+  uri: process.env.NEXT_APP_GQL_USER_API ?? 'http://localhost:3000/graphql',
+  credentials: 'include',
+})
 
-// const uploadLinkAuth = authUserLink.concat(uploadLink)
 
-// export const userClientUpload = new ApolloClient({
-//   cache: new InMemoryCache(),
-//   link: uploadLinkAuth,
-// })
 
 export const userClient = new ApolloClient({
-  link: authUserLink.concat(httpUserLink),
+  link: ApolloLink.from([authUserLink, httpUserLink, uploadLink]),
   cache: new InMemoryCache(),
   credentials: 'include',
 })
