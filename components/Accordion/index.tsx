319c739dac3ebfcab8834ba5cae42dbfddbdb727
Raul Gavris <rg.raulgavris@gmail.com>
2023-10-31 12:42:23 +0200

    change animation on faq accordion

--- components/Accordion/index.tsx
+++ components/Accordion/index.tsx
@@ -13,16 +13,14 @@ export const Accordion = ({
   const [isActive, setIsActive] = useState(false)
 
   return (
-    <div className="w-[1300px] flex flex-col">
+    <div className="w-[1000px] flex flex-col border-b border-white">
       <button
         type="button"
         onClick={() => {
           console.log('clicked')
           setIsActive(!isActive)
         }}
-        className={`${
-          isActive ? 'border-transparent' : 'border-white'
-        } flex items-center justify-between w-full py-5 font-medium text-left text-white border-b`}
+        className={`flex items-center justify-between w-full py-5 font-medium text-left text-white`}
       >
         <span className="text-[22px] leading-[33px]">{title}</span>
         <AddSvg className="w-7 h-7 rounded-lg" />
@@ -33,7 +31,7 @@ export const Accordion = ({
         transition={{ duration: 0.25 }}
         className={`overflow-hidden`}
       >
-        <div className="p-5 border-b border-white">{content}</div>
+        <div className="p-5">{content}</div>
       </motion.div>
     </div>
   )
