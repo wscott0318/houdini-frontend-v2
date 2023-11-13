48428af95722c4a4555281040de6e569fea9a176
Raul Gavris <rg.raulgavris@gmail.com>
2023-11-13 11:22:55 +0200

    change english to uk flag

--- lib/locales/languages.ts
+++ lib/locales/languages.ts
@@ -5,12 +5,13 @@ import roFlag from '@/assets/flags/ro.jpg'
 import russia from '@/assets/flags/russia.jpg'
 import spain from '@/assets/flags/spain.jpg'
 import usFlag from '@/assets/flags/us.jpg'
+import ukFlag from '@/assets/flags/uk.png'
 import frFlag from '@/assets/flags/french.jpg'
 
 const languages: any = {
   en: {
     label: 'English',
-    flag: usFlag,
+    flag: ukFlag,
   },
   fr: {
     label: 'French',
