c18180273097fdcd6a4ca003408c1124457403ac
Raul Gavris <rg.raulgavris@gmail.com>
2023-12-23 13:51:38 +0200

    fixes for powered by xblock

--- components/Footer/Logo/index.tsx
+++ components/Footer/Logo/index.tsx
@@ -8,10 +8,19 @@ import { FooterXBlockSvg, LogoSvg } from '../../Svg'
 
 export const Logo = ({ isHeader = false }: { isHeader?: boolean }) => {
   const router = useRouter()
+
+  function handleClick(event: React.MouseEvent) {
+    const target = event.target as HTMLElement
+    console.log('target', target)
+    if (target && target.id !== 'xblock') {
+      router.push('/')
+    }
+  }
+
   return (
     <div
-      onClick={() => {
-        router.push('/')
+      onClick={(e) => {
+        handleClick(e)
       }}
       className="hover:cursor-pointer flex flex-col gap-2 justify-center items-center"
     >
@@ -20,9 +29,13 @@ export const Logo = ({ isHeader = false }: { isHeader?: boolean }) => {
       ) : null}
       <LogoSvg className="w-[200px] h-[80px] fill-white" />
       {!isHeader ? (
-        <div className="z-[1] justify-start items-center">
-          <Link className="group flex flex-row" href="#">
-            <FooterXBlockSvg className="w-[128px] h-[26px]" />
+        <div className="flex justify-start items-center">
+          <Link
+            target="_blank"
+            className="group flex flex-row"
+            href="https://www.xblock.tech/"
+          >
+            <FooterXBlockSvg id="xblock" className="w-[128px] h-[26px]" />
           </Link>
         </div>
       ) : null}
