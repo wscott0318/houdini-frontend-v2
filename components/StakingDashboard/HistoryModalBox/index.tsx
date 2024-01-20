3f2087754a688581e58f268fedd271fdba1e3fcf
wscott <wscott0318@gmail.com>
2024-01-20 12:46:28 +0200

    fixes

--- components/StakingDashboard/HistoryModalBox/index.tsx
+++ components/StakingDashboard/HistoryModalBox/index.tsx
@@ -20,13 +20,14 @@ const eventTypes = {
   FallenWizardFee: t("FallenWizardFee"),
   MultiplierChanged: t("MultiplierChanged"),
 }
-const buttonNames = [t("all"),...Object.values(eventTypes)];
+const buttonNames = [t("all"), ...Object.values(eventTypes)];
 
 const HistoryModalBox = () => {
   const { address } = useAccount();
   const publicClient = usePublicClient();
 
   const [fromBlock, setFromBlock] = useState<bigint>(0n);
+  const [events, setEvents] = useState<IGroupedData[]>([]);
 
   useEffect(() => {
     publicClient.getBlockNumber().then((data: bigint) => setFromBlock(data - 800n));
@@ -122,63 +123,65 @@ const HistoryModalBox = () => {
     receiptData: false,
   });
 
-  let allEvents: any[] = [];
-  stakedEvents?.map((event: any) => {
-    allEvents.push(event);
-  });
-  withdrawnEvents?.map((event: any) => {
-    allEvents.push(event);
-  });
-
-  rewardPaidEvents?.map((event: any) => {
-    allEvents.push(event);
-  });
-
-  requestUnlockEvents?.map((event: any) => {
-    allEvents.push(event);
-  });
-
-  fallenWizardFeeEvents?.map((event: any) => {
-    allEvents.push(event);
-  });
-
-  multiplierChangedEvents?.map((event: any) => {
-    allEvents.push(event);
-  });
-
-  allEvents = allEvents.sort((a: any, b: any) => {
-    if (a?.block?.timestamp < b?.block?.timestamp) {
-      return -1; // a should be sorted before b
-    } else if (a?.block?.timestamp > b?.block?.timestamp) {
-      return 1; // a should be sorted after b
-    } else {
-      return 0; // a and b are equal
-    }
-  });
-
-  const events: IGroupedData[] = [];
-   allEvents.map(v => {
-    events.push({
-      type: v.log.eventName,
-      date: new Date(Number(v.block.timestamp) * 1000).toLocaleString('en-US', {
-        year: 'numeric',
-        month: 'long',
-        day: 'numeric',
-        hour: 'numeric',
-        minute: 'numeric',
-        second: 'numeric'
-      }),
-      amount: Math.round(parseFloat(formatUnits(v.args.amount, 18))).toString(),
-      addressUp: v.transaction.from,
-      addressDown: v.block.hash,
+  useEffect(() => {
+    let allEvents: any[] = [];
+    stakedEvents?.map((event: any) => {
+      allEvents.push(event);
+    });
+    withdrawnEvents?.map((event: any) => {
+      allEvents.push(event);
+    });
+  
+    rewardPaidEvents?.map((event: any) => {
+      allEvents.push(event);
+    });
+  
+    requestUnlockEvents?.map((event: any) => {
+      allEvents.push(event);
+    });
+  
+    fallenWizardFeeEvents?.map((event: any) => {
+      allEvents.push(event);
+    });
+  
+    multiplierChangedEvents?.map((event: any) => {
+      allEvents.push(event);
+    });
+  
+    allEvents = allEvents.sort((a: any, b: any) => {
+      if (a?.block?.timestamp < b?.block?.timestamp) {
+        return -1; // a should be sorted before b
+      } else if (a?.block?.timestamp > b?.block?.timestamp) {
+        return 1; // a should be sorted after b
+      } else {
+        return 0; // a and b are equal
+      }
+    });
+    allEvents.map(v => {
+      setEvents([...events
+        , {
+        type: v?.log?.eventName,
+        date: new Date(Number(v?.block?.timestamp) * 1000).toLocaleString('en-US', {
+          year: 'numeric',
+          month: 'long',
+          day: 'numeric',
+          hour: 'numeric',
+          minute: 'numeric',
+          second: 'numeric'
+        }),
+        amount: Math.round(parseFloat(formatUnits(v?.args?.amount ?? 0n, 18))).toString(),
+        addressUp: v.transaction?.from,
+        addressDown: v.block?.hash,
+      }
+    ]);
     })
-  })
+  },[fallenWizardFeeEvents, multiplierChangedEvents, requestUnlockEvents, rewardPaidEvents, stakedEvents, withdrawnEvents] )
 
   const [headValue, setHeadValue] = useState(0)
 
-  if (!address) {
-    return <h1>Please connect your wallet first</h1>;
-  }
+  // if (!address) {
+  //   return <div className='p-4 mb-4 w-[100%] text-sm text-white rounded-lg bg-orange-900 dark:bg-gray-800 dark:text-red-400'>Please connect your wallet first</div>;
+  // }
   return (
     <div className="flex flex-col items-center backdrop-blur-[46px] custom-modal-step2-drop-shadow rounded-[28px] w-[1071px] p-[1px] z-[1]">
       <div className="w-full h-full p-[30px] rounded-[28px] custom-balances-box-inner-shadow relative">
@@ -192,7 +195,7 @@ const HistoryModalBox = () => {
         <div className="flex flex-col gap-[20px]">
           <span className="rainbow-text text-[18px] font-medium">History</span>
           {/* <ButtonGroup names={buttonNames} /> */}
-          <div className="flex flex-col">
+          <div className="flex flex-col min-h-[400px]">
             <div className="flex flex-row px-[21px] py-[20px] gap-[178px] relative items-center">
               {/* <div className="absolute top-[26px] left-[27px]">
                 <CheckBox />
@@ -211,7 +214,7 @@ const HistoryModalBox = () => {
               </span>
             </div>
             <div className="flex flex-col">
-              {events.map((item, index) => (
+              {events && events.map((item, index) => (
                 <div key={index}>
                   <ListLine data={item} index={index % 2} />
                 </div>
