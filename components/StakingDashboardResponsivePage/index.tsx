083a6dd62c27d14ddc5cfbc4ddc27655fd8004ac
dreamworker <dreamworktalent@gmail.com>
2024-01-20 10:57:03 -0800

    Modify Connect Wallet Button

--- components/StakingDashboardResponsivePage/index.tsx
+++ components/StakingDashboardResponsivePage/index.tsx
@@ -19,26 +19,7 @@ export default function StakingDashboardResponsivePage({
       </div>
       <div className="flex flex-col w-full">
         <div className="flex px-[50px] pt-[30px] justify-end gap-[24px]">
-          {/* <CTAButton width="191px" height="44px">
-            <span className="px-[30px] py-[12px] justify-center items-center">
-              Disconnect Wallet
-            </span>
-          </CTAButton>
-          <div className="inline-flex items-center justify-center p-[2px] rounded-[120px] bg-gradient-to-b from-white to-black">
-            <div className="inline-flex justify-center rounded-[120px] w-full h-full items-center bg-gradient-to-br from-black to-[#252932] px-[30px] py-[8px]">
-              <div className="flex flex-row gap-[10px]">
-                <span className="text-[16px] font-semibold leading-normal">
-                  0xeed9978.....e372b0154
-                </span>
-                <Image
-                  src={avatar}
-                  className="lg:w-[24px] lg:h-[24px]"
-                  alt="avatar"
-                />
-              </div>
-            </div>
-          </div> */}
-          {/* <ConnectWalletAccount user={user} token={token} staker={deployedStakerData} approved={approved} timeLeft={timeLeft}/> */}
+          <ConnectWalletAccount />
         </div>
         {children}
       </div>
